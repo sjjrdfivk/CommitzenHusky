65d8f7b6e552964d55e62759e4859fe4d2bfb98e
Rex.Q <401810445@qq.com>
2024-02-26 17:34:03 +0800

    test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App () {
+  const test = ""
   return (
     <div className="App">
       <header className="App-header">
